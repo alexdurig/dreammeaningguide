c368cf91504f94923f516ce272bb36785023a484
alexdurig <alex.durig@gmail.com>
2026-07-21 14:51:55 -0400

    Simplify sitemap integration configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,6 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://dreammeaningguide.site',
-
-  integrations: [
-    sitemap({
-      changefreq: 'weekly',
-      priority: 0.7,
-      lastmod: new Date(),
-      filter: (page) =>
-        !page.includes('/journal') &&
-        !page.includes('/404'),
-    }),
-  ],
-
+  integrations: [sitemap()],
   output: 'static',
 });
-
-

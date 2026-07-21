63a59c042a054549b881f2da3d1731844f337c19
alexdurig <alex.durig@gmail.com>
2026-07-21 16:28:32 -0400

    Add sitemap integration to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://dreammeaningguide.site',
+  integrations: [sitemap()],
   output: 'static',
 });
-
